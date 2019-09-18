e3306cea39df76602f0fcfd123a61acf28e67029
SumitBharsawade <sumitpatil91198@gmail.com>
2019-09-18 21:42:54 +0530

    record entry code added

--- website/index.py
+++ website/index.py
@@ -1,11 +1,28 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request,flash
+from recordEnterFrom import RecordEnterForm
 app=Flask(__name__)
-@app.route('/')
+app.config['SECRET_KEY']='b4c3f4b70ec9b4e0' #protect against key and attacks
+@app.route('/',methods=['GET', 'POST'])
 @app.route('/index')
 def index():
-    user = {'username': 'sumit'}
-    return render_template('/Enter_Record/recordEnter.html')
+    form=RecordEnterForm(request.form)
+    if request.method == 'POST':
+        broker_name=request.form['broker_name']
+        company_name=request.form['company_name']
+        current_price=request.form['current_price']
+        recommanded_price=request.form['recommanded_price']
+        predict_date=request.form['predict_date']
+        target_price=request.form['target_price']
+        close_price=request.form['close_price']
+        print(broker_name, " ", company_name, " ", current_price, " ", recommanded_price, " ", predict_date, " ", target_price, " ", close_price)
+    
+        if form.validate():
+        # Save the comment here.
+            flash('Record added Successfully')
+        else:
+            flash('Error: occurred ')
+        flash(' ')
+    return render_template('/Enter_Record/recordEnter.html',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ website/recordEnterFrom.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,FloatField,DateField,SubmitField
+from wtforms.validators import DataRequired,Length,EqualTo
+
+class RecordEnterForm(FlaskForm):
+    broker_name=StringField('Broker Name',validators=[DataRequired(),Length(min=1)])
+    company_name=StringField('Company Name',validators=[DataRequired(),Length(min=1)])
+    current_price=FloatField('Current Price',validators=[DataRequired()])
+    recommanded_price=FloatField('Recommanded Price',validators=[DataRequired()])
+    predict_date=DateField('Predict Date',validators=[DataRequired()])
+    target_price=FloatField('Target Price',validators=[DataRequired()])
+    close_price=FloatField('Close Price',validators=[DataRequired()])
+    submit=SubmitField('Submit')
