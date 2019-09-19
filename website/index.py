ad578f653d1b6bf4ad10589cfca44b97d24a1391
Yash Bhandare <yashbhandare1708@gmail.com>
2019-09-19 14:34:26 +0530

    UI form connected to test db for manual data entry - yash

--- website/index.py
+++ website/index.py
@@ -1,5 +1,14 @@
 from flask import Flask,render_template,request,flash
 from recordEnterFrom import RecordEnterForm
+import sqlalchemy
+from datetime import datetime
+import mysql.connector
+from sqlalchemy import create_engine
+import numpy as np
+import pandas as pd
+from sqlalchemy.sql import text
+
+
 app=Flask(__name__)
 app.config['SECRET_KEY']='b4c3f4b70ec9b4e0' #protect against key and attacks
 @app.route('/',methods=['GET', 'POST'])
@@ -10,18 +19,27 @@ def index():
         broker_name=request.form['broker_name']
         company_name=request.form['company_name']
         current_price=request.form['current_price']
-        recommanded_price=request.form['recommanded_price']
+        recomended_price=request.form['recomended_price']
         predict_date=request.form['predict_date']
         target_price=request.form['target_price']
         close_price=request.form['close_price']
-        print(broker_name, " ", company_name, " ", current_price, " ", recommanded_price, " ", predict_date, " ", target_price, " ", close_price)
-        #Write database addition code here
+        print(broker_name, " ", company_name, " ", current_price, " ", recomended_price, " ", predict_date, " ", target_price, " ", close_price)
+        
+        
+        
+        database_username = 'root'
+        database_password = 'yash'
+        database_ip       = '127.0.0.1'
+        database_name     = 'mainschema'
+        database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.format(database_username, database_password, database_ip, database_name))
+
         if form.validate():
         # Save the comment here.
             flash('Record added Successfully')
+            result = database_connection.execute(text("INSERT INTO test(broker, company_name, current_price, recomended_buying, predict_date, target_price, close_price) VALUES ('"+broker_name+"','"+company_name+"','"+current_price+"','"+recomended_price+"','"+predict_date+"','"+target_price+"','"+close_price+"');"))
+
         else:
             flash('Error: occurred ')
-        flash(' ')
     return render_template('/Enter_Record/recordEnter.html',form=form)
 
 if __name__=='__main__':
--- website/recordEnterFrom.py
+++ website/recordEnterFrom.py
@@ -6,7 +6,7 @@ class RecordEnterForm(FlaskForm):
     broker_name=StringField('Broker Name',validators=[DataRequired(),Length(min=1)])
     company_name=StringField('Company Name',validators=[DataRequired(),Length(min=1)])
     current_price=FloatField('Current Price',validators=[DataRequired()])
-    recommanded_price=FloatField('Recommanded Price',validators=[DataRequired()])
+    recomended_price=FloatField('Recomended Price',validators=[DataRequired()])
     predict_date=DateField('Predict Date',validators=[DataRequired()])
     target_price=FloatField('Target Price',validators=[DataRequired()])
     close_price=FloatField('Close Price',validators=[DataRequired()])
