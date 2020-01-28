67be73f672b607bbe9b34b7a28f82e86a243619a
SumitBharsawade <sumitpatil91198@gmail.com>
2020-01-28 13:07:10 +0530

    api update

--- website/api/api.py
+++ website/api/api.py
@@ -6,10 +6,18 @@ app=Flask(__name__)
 # cnx = mysql.connector.connect(user="smarsproddbuser@mbt-smars-mysql", password='modern@1234', host="mbt-smars-mysql.mysql.database.azure.com", port=3306, database='mainschema', ssl_disabled=True)
 cnx = mysql.connector.connect(user="root", password='yash', host="127.0.0.1", port=3306, database='mainschema', ssl_disabled=True)
 
-@app.route('/getbrokerinfo',methods=['GET'])
+@app.route('/getbrokerinfo/<string:year>',methods=['GET'])
 def sendBrokerInfoApi():
     mycursor = cnx.cursor()
-    sqlquery = "SELECT broker,Rating FROM finalrating"
+    if year == '2016' :
+        sqlquery = "SELECT * FROM finalrating2016"
+        elif year == '2017' :
+            sqlquery = "SELECT * FROM finalrating2017"
+        elif year == '2018' :
+            sqlquery = "SELECT * FROM finalrating2018"
+        else :
+            sqlquery = "SELECT * FROM finalrating"
+    
     mycursor.execute(sqlquery)
     data = mycursor.fetchall()
     print(type(data))
